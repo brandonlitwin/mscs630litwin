f9d5b1096fd350711fdd31afeaed9ca8146e8242
Brandon Litwin <brandon.litwin1@marist.edu>
2020-01-27 11:27:37 -0500

    Initial setup of driver with comments

---
+++ prj/Driver_lab1.java
@@ -0,0 +1,35 @@
+/** 
+ * file: Driver_lab1.java
+ * author: Brandon Litwin
+ * course: MSCS 630
+ * assignment: Lab 1
+ * due date: 2/2/2020
+ * version: 1.0
+ *
+ * This file contains the declaration of the driver 
+ * for the Lab 1 cryptography algorithm.
+ */
+
+ /** 
+  * Driver_lab1
+  *
+  * This class reads an input String from 'a' to 'z' or 'A' to 'Z' and
+  * calls the str2int method to transform it into integers from 0 to 25, 
+  * with a space becoming 26.
+  */
+ public class Driver_lab1 {
+
+ }
+ /** 
+  * str2int
+  * This method takes an input String and reads each character one at 
+  * a time, and then converts it into an array of integers to be returned.
+  *
+  * Parameters: 
+  *   plaintext: the String taken from a line of input.
+
+  * Return value: an integer array containing the encrypted plaintext.
+  */
+ public class int[] str2int(String plaintext) {
+
+ }
