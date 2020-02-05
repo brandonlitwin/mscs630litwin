1e13685623fe1077507610eadfba5c8f1f233652
Brandon Litwin <brandon.litwin1@marist.edu>
2020-02-05 17:08:03 -0500

    Partial solution

--- prj/Driver_lab2b.java
+++ prj/Driver_lab2b.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * file: Driver_lab2b.java
  * author: Brandon Litwin
  * course: MSCS 630
@@ -12,8 +12,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-/** 
- * Driver_lab2a
+/**
+ * Driver_lab2b
  *
  * This class reads two integers (a and b) and calls the euclidAlgExt method to
  * compute the resulting values of the Extended Euclidean algorithm.
@@ -23,7 +23,7 @@ public class Driver_lab2b {
    * main
    *
    * This class reads an input consisting of one or more lines of two integers,
-   * and calls the euclidAlgExt method to calculate d, x, and y to satify the 
+   * and calls the euclidAlgExt method to calculate d, x, and y to satify the
    * equation d = ax+by where d = gcd(a,b).
    * Then, it outputs these values as an array of long integers.
    *
@@ -33,75 +33,83 @@ public class Driver_lab2b {
    *   currentSecondInt: The second integer of the current line of input.
    *   currentEuclidResultArray: The result array of the euclidAlgExt method on
    *                             the current line of input.
-   *   resultOutput: An instance of StringBuilder which is used to 
+   *   resultOutput: An instance of StringBuilder which is used to
    *                    convert the long array to a string for output.
-   * 
+   *
    * Return value: A System output containing the GCD of each input line.
    */
   public static void main(String[]args) {
-    try (BufferedReader reader = new 
-         BufferedReader(new InputStreamReader(System.in))) {
-      String inputLine = "";
-      while ((inputLine = reader.readLine()) != null) {
-        long currentFirstInt = Long.parseLong(inputLine.split(" ")[0]);
-        long currentSecondInt = Long.parseLong(inputLine.split(" ")[1]);
-        long[] currentEuclidResultArray = 
-               euclidAlgExt(currentFirstInt, currentSecondInt);
-        StringBuilder resultOutput = new StringBuilder();
-        for (int i = 0; i < currentEuclidResultArray.length; i++) {
-          resultOutput.append(currentEuclidResultArray[i] + " ");
+    try (BufferedReader reader = new
+            BufferedReader(new InputStreamReader(System.in))) {
+        String inputLine = "";
+        while ((inputLine = reader.readLine()) != null) {
+            long currentFirstInt = Long.parseLong(inputLine.split(" ")[0]);
+            long currentSecondInt = Long.parseLong(inputLine.split(" ")[1]);
+            long[] currentEuclidResultArray =
+                    euclidAlgExt(currentFirstInt, currentSecondInt);
+            StringBuilder resultOutput = new StringBuilder();
+            for (int i = 0; i < currentEuclidResultArray.length; i++) {
+                resultOutput.append(currentEuclidResultArray[i] + " ");
+            }
+            System.out.println(resultOutput.toString().trim());
         }
-        System.out.println(resultOutput.toString().trim());
-      }
     } catch (IOException e) {
         e.printStackTrace();
     }
   }
-
-  /** 
+  /**
    * euclidAlg
-   * 
-   * This class takes two integers and computes the values of the 
+   *
+   * This class takes two integers and computes the values of the
    * Extended Euclidean algorithm d, x, and y, where
-   * d = ax+by where d = gcd(a,b).
-   * The Extended Euclidean algorithm adds the following procedures to the 
-   * standard Euclidean algorithm:
+   * d = ax+by where d = gcd(a,b)
    *
-   *
-   * Parameters: 
+   * Parameters:
    *   a: the first input integer.
    *   b: the second input integer.
-   *   tempHolder: the temporary holder of b in case a and b need to 
+   *   tempHolder: the temporary holder of b in case a and b need to
    *               be switched.
-   *   k: the integer that divides a and b.
+   *   k: the integer that holds the result of a % b.
+   *   x: the integer that holds the current value of x,
+   *      the first Bezout number
+   *   y: the integer that holds the current value of y,
+   *      the second Bezout number
+   *   oldX: the previous integer value of x
+   *   oldY: the previous integer value of y
+   *   quotientFloor: the integer result of doing floor division of b / a
+   *   m: the current value of x - oldX * quotientFloor
+   *   n: the current value of y - oldY * quotientFloor
    *   gcd: the integer value of the greatest common divisor of a and b.
    *
-   * Return value: gcd, the integer value of the greatest common divisor 
+   * Return value: gcd, the integer value of the greatest common divisor
    *               of a and b.
    */
   public static long[] euclidAlgExt(long a, long b) {
     long[] results = new long[3];
     long gcd = 0;
     long x = 0;
-    long y = 0;
-    long k = 0;
+    long y = 1;
+    long oldX = 1;
+    long oldY = 0;
     // switch values of a and b if b > a
     if (b > a) {
-      long tempHolder = b;
-      b = a;
-      a = tempHolder;
+        long tempHolder = b;
+        b = a;
+        a = tempHolder;
     }
-    while (b > 0) {
-      k = a % b;
-      a = b;
-      b = k;
+    while (a > 0) {
+        long quotientFloor = Math.floorDiv(b,a);
+        long k = b % a;
+        long m = x - oldX * quotientFloor;
+        long n = y - oldY * quotientFloor;
+        b = a;
+        a = k;
+        x = oldX;
+        y = oldY;
+        oldX = m;
+        oldY = n;
     }
-    gcd = a;
-    // calculate x and y
-    // x = y1 - b/a * x1
-    //   =    - 75/148
-    // y = x1
-    x = 
+    gcd = b;
     results[0] = gcd;
     results[1] = x;
     results[2] = y;
