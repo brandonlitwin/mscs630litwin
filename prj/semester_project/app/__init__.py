d24c53579bbcc83ec10d77dfaf4f1557ffa5ed23
Brandon Litwin <brandon.litwin1@marist.edu>
2020-03-25 18:54:22 -0400

    Add templates and basic login form

--- prj/semester_project/app/__init__.py
+++ prj/semester_project/app/__init__.py
@@ -1,41 +1,7 @@
-import os
-
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from app import routes
-
-"""# a simple page that says hello
-@app.route('/')
-def hello():
-    return 'Hello, World!'
-
-if __name__ == "main":
-    app.run(host='0.0.0.0', port=8080, debug=True)"""
-
-
-"""def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'panpan.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    
-
-    return app"""
